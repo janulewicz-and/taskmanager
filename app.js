79123f3513bde1956dad98f98b3a22576ef31889
Andrzej_Janulewicz <and.yanulevich@gmail.com>
2025-04-29 13:06:34 +0200

    calc 1.0.0

--- app.js
+++ app.js
@@ -2,33 +2,46 @@
  * @param {HTMLElement} id
  */
 function app(element) {
-  const startNumericInput = document.createElement("input");
-  startNumericInput.placeholder = "Enter A";
+  const startNumericInput = document.createElement('input');
+  startNumericInput.placeholder = 'Enter A';
 
-  const endNumericInput = document.createElement("input");
-  endNumericInput.placeholder = "Enter B";
+  const endNumericInput = document.createElement('input');
+  endNumericInput.placeholder = 'Enter B';
 
   // TODO: not implemented
-  const mathSignInput = document.createElement("input");
-  mathSignInput.placeholder = "Enter a sign";
+  const mathSignInput = document.createElement('input');
+  mathSignInput.placeholder = 'Enter a sign';
 
-  const mathResultElement = document.createElement("div");
+  const mathResultElement = document.createElement('div');
 
-  const calcButton = document.createElement("button");
-  calcButton.textContent = "=";
+  const calcButton = document.createElement('button');
+  calcButton.textContent = '=';
   // TODO: not implemented
   calcButton.onclick = () => {
-    mathResultElement.textContent =
-      startNumericInput.value - endNumericInput.value;
+    numA = +startNumericInput.value;
+    numB = +endNumericInput.value;
+    if (mathSignInput.value === '/') {
+      mathResultElement.textContent = numA / numB;
+    }
+    if (mathSignInput.value === '*') {
+      mathResultElement.textContent = numA * numB;
+    }
+    if (mathSignInput.value === '+') {
+      mathResultElement.textContent = numA + numB;
+    }
+    if (mathSignInput.value === '-') {
+      mathResultElement.textContent = numA - numB;
+    }
   };
 
   element.appendChild(startNumericInput);
+  element.appendChild(mathSignInput);
   element.appendChild(endNumericInput);
   element.appendChild(calcButton);
   element.appendChild(mathResultElement);
 }
 
-const appElement = document.getElementById("app");
+const appElement = document.getElementById('app');
 
 if (appElement) {
   app(appElement);
