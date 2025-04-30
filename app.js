13da429fe792ff52877bfd2e766e4acfa55b9a2a
Andrzej_Janulewicz <and.yanulevich@gmail.com>
2025-04-30 13:41:58 +0200

    calc 1.0.5

--- app.js
+++ app.js
@@ -8,39 +8,66 @@ function app(element) {
   const endNumericInput = document.createElement('input');
   endNumericInput.placeholder = 'Enter B';
 
-  // TODO: not implemented
-  const mathSignInput = document.createElement('input');
-  mathSignInput.placeholder = 'Enter a sign';
+  const mathSignInput = document.createElement('select');
+  ['+', '-', '*', '/'].forEach((sign) => {
+    const option = document.createElement('option');
+    option.value = sign;
+    option.textContent = sign;
+    mathSignInput.appendChild(option);
+  });
 
   const mathResultElement = document.createElement('div');
 
   const calcButton = document.createElement('button');
   calcButton.textContent = '=';
-  // TODO: not implemented
-  calcButton.onclick = () => {
-    numA = +startNumericInput.value;
-    numB = +endNumericInput.value;
-    if (mathSignInput.value === '/') {
-      mathResultElement.textContent = numA / numB;
+  const style = document.createElement('style');
+  style.textContent = `
+    .input-error {
+      border: 2px solid red;
+      background-color:rgb(233, 0, 0);
     }
-    if (mathSignInput.value === '*') {
-      mathResultElement.textContent = numA * numB;
-    }
-    if (mathSignInput.value === '+') {
-      mathResultElement.textContent = numA + numB;
-    }
-    if (mathSignInput.value === '-') {
-      mathResultElement.textContent = numA - numB;
-    }
-  };
+  `;
+  document.head.appendChild(style);
 
+  [startNumericInput, endNumericInput].forEach((input) => {
+    input.addEventListener('input', () => {
+      const val = input.value;
+      if (isNaN(Number(val)) || val.trim() === '') {
+        input.classList.add('input-error');
+      } else {
+        input.classList.remove('input-error');
+      }
+    });
+  });
+
+  calcButton.onclick = () => {
+    numA = Number(startNumericInput.value);
+    numB = Number(endNumericInput.value);
+    const sign = mathSignInput.value;
+    if (
+      isNaN(numA) ||
+      isNaN(numB) ||
+      typeof numA !== 'number' ||
+      typeof numB !== 'number'
+    ) {
+      mathResultElement.textContent = 'Введите корректные числа';
+      return;
+    }
+
+    let result;
+    if (sign === '/') result = numA / numB;
+    if (sign === '*') result = numA * numB;
+    if (sign === '+') result = numA + numB;
+    if (sign === '-') result = numA - numB;
+
+    mathResultElement.textContent = `${result}`;
+  };
   element.appendChild(startNumericInput);
   element.appendChild(mathSignInput);
   element.appendChild(endNumericInput);
   element.appendChild(calcButton);
   element.appendChild(mathResultElement);
 }
-
 const appElement = document.getElementById('app');
 
 if (appElement) {
